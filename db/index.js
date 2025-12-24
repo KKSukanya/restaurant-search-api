777ab6577955c7856a8536be1a9fa0386a72073d
K K Sukanya <165251827+KKSukanya@users.noreply.github.com>
2025-12-24 14:53:14 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,24 +1,23 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
     console.log("Database connected successfully!");
-  } catch (err) {
-    console.error("Database connection failed:", err);
+  } catch (error) {
+    console.error("Database connection failed:", error);
   }
 };
 
