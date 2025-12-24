31b8f5cdbed0e92cc5e08aeb7504cc8791989d72
K K Sukanya <165251827+KKSukanya@users.noreply.github.com>
2025-12-24 15:25:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-
-const { testConnection } = require("./db/index");
+const {sequelize, testConnection } = require("./db/index");
 const searchRoutes = require("./routes/search");
+require("dotenv").config();
 
 app.use(express.json());
 
 // Routes
 app.use("/search", searchRoutes);
 
-// IMPORTANT: Railway-compatible PORT handling
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", async () => {
+app.listen(PORT, async () => {
   await testConnection();
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
